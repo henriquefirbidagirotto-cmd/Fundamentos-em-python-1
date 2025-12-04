1c9006f3cbfde3a65045950d932b2fad5520b337
henriquefirbidagirotto-cmd <henriquefirbidagirotto@gmail.com>
2025-12-04 17:37:57 -0300

    Add files via upload

---
+++ Aula2_atv2.py
@@ -0,0 +1,18 @@
+#1 - Crie um programa para efetuar a leitura de um número real e apresentar o resultado do cubo deste número.
+n1 = int(input('Digite um numero:'))
+n2 = n1 ** 3
+print(n2)
+#2 - Crie 3 variáveis para armazenar nomes de frutas. Em seguida, concatene-as .
+f1 = str(input('Fruta 1:'))
+f2 = str(input('Fruta 2:'))
+f3 = str(input('Fruta 3:'))
+print(f1, f2, f3)
+#3 - Peça ao usuário para digitar dois números inteiros e armazene-os em variáveis. Realize a concatenação desses números.
+nu1 = int(input('Digite um numero:'))
+nu2 = int(input('Digite um numero:'))
+print(nu1)
+print(nu2)
+#4 - Crie uma variável para armazenar a palavra "Python é show". Em seguida, adicione um dado boleano ao final da palavra usando a concatenação e exiba o resultado.
+py = str('Python é show')
+bo = bool(1)
+print(py, bo)
